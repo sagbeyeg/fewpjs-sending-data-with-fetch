f622f4b2fef15ad57902f755fea1daece605de2f
sagbeyeg <agbeyegbesisan@gmail.com>
2020-10-29 19:16:33 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData (name, email) {
+  let formData = {
+    name: name,
+    email: email
+  };
+  
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+}
+
+
+  return fetch('http://localhost:3000/users', configObj)
+  .then(function (resp) { 
+    return resp.json()
+  })
+  .then(function (obj) {
+     console.log(obj)
+     document.body.innerHTML = obj.id + ' - ' + obj.name + ' - ' + obj.email
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+    document.body.innerHTML = error.message
+  });
+}
+
+//test
+// submitData("Sisan", "sisan@sisan.com")
